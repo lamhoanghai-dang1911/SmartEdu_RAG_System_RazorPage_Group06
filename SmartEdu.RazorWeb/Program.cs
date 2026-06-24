02925bb4b249646321941937fd8d1faafbb4e21c
Vũ Thanh Hải <haithanhvu0501@gmail.com>
2026-06-24 08:28:25 +0700

    Update Program.cs

--- SmartEdu.RazorWeb/Program.cs
+++ SmartEdu.RazorWeb/Program.cs
@@ -22,7 +22,6 @@ namespace SmartEdu.RazorWeb
                 options.UseSqlServer(connectionString));
 
             // 2. Đăng ký các dịch vụ Business Layer (Nơi chứa ChatService, etc.)
-            // Bạn tạo một class "DependencyInjection.cs" trong tầng Business để quản lý
 
 
             
